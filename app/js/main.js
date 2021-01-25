0ec7097f23951791d1ff31e1ba236dc16614a189
peschernuy <peschernuy@gmail.com>
2021-01-25 22:30:45 +0200

    add team slider slick

--- app/js/main.js
+++ app/js/main.js
@@ -9,15 +9,40 @@ $(function(){
   });
 
 
+  $('.team__photo-box').slick({
+    slidesToShow: 6,
+    asNavFor: '.team__slider',
+    focusOnSelect: true,
+    arrows: false
+    
+  })
+
+
+
+
+
   $('.team__slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: false,
-    arrows: true
+    arrows: true,
+    asNavFor: '.team__photo-box'
+    
   });
+
+
+ 
+
+
+
+
+
+
 
 $('.menu__btn').on('click', function(){
   $('.menu__list').toggleClass('menu__list--active');
+
+
 });
 
 

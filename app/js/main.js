3d3d469ddc88138d2ce4f82c5200cac3f685876b
peschernuy <peschernuy@gmail.com>
2021-01-28 19:28:06 +0200

    add footer adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -5,9 +5,27 @@ $(function(){
     slidesToShow: 3,
     slidesToScroll: 3,
     dots: false,
-    arrows:true
+    arrows:true,
 
-    
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 2,
+        }
+        
+
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+
+        }
+      }
+      
+
+    ]
 
 
 
